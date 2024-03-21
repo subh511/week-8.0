e4b3eea51b03a0fb022200127922aae9e3e1e3f6
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-21 17:57:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import {React} from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-between'>
+    <h1>child</h1>
+    <h2>childs</h2>
+    <h3>children</h3>
+    <h4>childrens</h4>
+    </div>
+  )
+}
+
+export default App
